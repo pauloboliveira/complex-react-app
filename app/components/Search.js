06ce4e18e6d62b3a1dfe8ad4965a952fd7e552b7
Paulo Borges <pborges086@hotmail.com>
2020-11-25 22:03:54 -0300

    Implementation of search request 1

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,8 +1,15 @@
 import React, { useContext, useEffect } from "react"
 import DispatchContext from "../DispatchContext";
+import { useImmer } from "use-immer";
 
 function Search() {
   const appDispatch = useContext(DispatchContext)
+  const [state, setState] = useImmer({
+    searchTerm: '',
+    results: [],
+    show: 'neither',
+    requestCount: 0
+  })
 
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler)
@@ -10,10 +17,34 @@ function Search() {
     return () => document.removeEventListener("keyup", searchKeyPressHandler)
   }, [])
 
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setState(draft => {
+        draft.requestCount++
+      })
+    }, 3000)
+
+    useEffect(() => {
+      if(state.requestCount){
+        // Send Axios request
+      }
+    }, [state.requestCount])
+
+    return () => clearTimeout(delay)
+  }, [state.searchTerm])
+
   function searchKeyPressHandler(e) {
     if (e.keyCode == 27) {
       appDispatch({ type: "closeSearch" })
     }
+  }
+
+  function handleInput(e) {
+    const value = e.target.value
+    setState(draft => {
+      draft.searchTerm = value
+    })
   }
 
   return (
@@ -23,7 +54,7 @@ function Search() {
           <label htmlFor="live-search-field" className="search-overlay-icon">
             <i className="fas fa-search"></i>
           </label>
-          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+          <input onChange={handleInput} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
           <span onClick={() => appDispatch({ type: "closeSearch" })} className="close-live-search">
             <i className="fas fa-times-circle"></i>
           </span>
