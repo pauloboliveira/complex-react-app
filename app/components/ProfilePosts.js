e8b5e04fb025955c691f56ad9f3b5bcabc05f18d
pauloboliveira <pborges086@hotmail.com>
2020-12-08 21:58:25 -0300

    Implement the home feed page

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -2,6 +2,7 @@ import Axios from "axios"
 import React, { useEffect, useState } from "react"
 import { useParams, Link } from 'react-router-dom'
 import LoadingDotIcon from "./LoadingDotsIcon";
+import Post from "./Post";
 
 function ProfilePosts() {
     const { username } = useParams()
@@ -32,16 +33,7 @@ function ProfilePosts() {
     return (
         <div className="list-group">
             {posts.map(post => {
-                const date = new Date(post.createdDate)
-                const dateFormatted = `${date.getMonth() + 1}/${date.getDay()}/${date.getFullYear()}`
-
-
-                return (
-                    <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
-                        <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> {" "}
-                        <span className="text-muted small">on {dateFormatted} </span>
-                    </Link>
-                )
+                return <Post post={post} key={post._id} noAuthor={true}/>
             })}
         </div>
     )
